ead5bd0faf9a74f5db7db14aa3b35b1db6defa3f
jtr-code <jtrtricks@gmail.com>
2024-09-19 12:20:24 +0530

    add : navbar

--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
-type Props = {}
-
-const DashboardWrapper = (props: Props) => {
+import React from "react";
+import Navbar from "@/app/(components)/Navbar";
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>DashboardWrapper</div>
-  )
-}
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* sidebar */}
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
