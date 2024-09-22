b23842f3fc79d687fee4e751262e0b9f55ff7698
jtr-code <jtrtricks@gmail.com>
2024-09-22 14:51:42 +0530

    fix : folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DashboardWrapper>
-        {children}
-      </DashboardWrapper>
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
